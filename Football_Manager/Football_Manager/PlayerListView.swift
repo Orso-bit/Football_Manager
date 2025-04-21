29e9bfd8939bd6e2872bc27ce28c9d729e12b49f
Orso-bit <giovannijr2000@gmail.com>
2025-04-21 22:21:27 +0200

    Player card completed

---
+++ Football_Manager/Football_Manager/PlayerListView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerListView.swift
+//  Football_Manager
+//
+//  Created by Giovanni Jr Di Fenza on 21/04/25.
+//
+
+import SwiftUI
+
+struct PlayerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerListView()
+}
